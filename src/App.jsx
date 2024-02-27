d7187ce0d4e7b2a15a907773eb32b3eb8fbe892d
Vaibhav Kumar Jha <vaibhavkumarjha46@gmail.com>
2024-02-27 18:02:30 +0530

    Splitting Components & Building Reusable Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import Player from "./Components/Player";
+
 function App() {
-  return <p>Comming Soon...</p>;
+  return (
+    <menu>
+      <div id="game-container">
+        <ol id="players">
+          <Player name="Player 1" symbol="X"/>
+          <Player name="Player 2" symbol="O"/>
+        </ol>
+        GAME BOARD
+      </div>
+      LOG
+    </menu>
+  );
 }
 
 export default App;
