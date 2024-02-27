fb327147a8f85b150b7a8c387cc0696c9d7aa3a7
Vaibhav Kumar Jha <vaibhavkumarjha46@gmail.com>
2024-02-27 19:10:17 +0530

    Conditional Content

--- src/Components/Player.jsx
+++ src/Components/Player.jsx
@@ -1,18 +1,23 @@
 import { useState } from "react";
 
 export default function Player({ name, symbol }) {
-    const [isEditing, setIsEditing] = useState(false)
-    function handleEditClick(){
-        setIsEditing(true)
-        console.log(isEditing)
-    }
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing(!isEditing);
+    console.log(isEditing);
+  }
+  let playerName = <span className="player-name">{name}</span>;
+  if (isEditing) {
+    playerName = <input type="text" required value={name} />;
+  }
   return (
     <li>
       <span>
-        {isEditing? <input type="text" required/> : <span className="player-name">{name}</span>}
+        {playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button onClick={handleEditClick}>Edit</button>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
     </li>
   );
 }
