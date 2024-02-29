b6ed63e436e331aae365a38dfce855d992d7de00
Vaibhav Kumar Jha <vaibhavkumarjha46@gmail.com>
2024-02-29 14:03:31 +0530

    Rendering Multi Dimensional Lists

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Player from "./Components/Player";
+import GameBoard from "./Components/GameBoard";
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
           <Player initialName="Player 1" symbol="X"/>
           <Player initialName="Player 2" symbol="O"/>
         </ol>
-        GAME BOARD
+        <GameBoard/>
       </div>
       LOG
     </menu>
---
+++ src/Components/GameBoard.jsx
@@ -0,0 +1,22 @@
+let initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+export default function GameBoard() {
+  return (
+    <ol id="game-board">
+      {initialGameBoard.map((row, rowIndex) => 
+          <li key={rowIndex}>
+            <ol>
+              {row.map((playerSymbol, colIndex) => 
+                 <button key={colIndex}>{playerSymbol}</button>
+              )}
+            </ol>
+          </li>
+        
+      )}
+    </ol>
+  );
+}
