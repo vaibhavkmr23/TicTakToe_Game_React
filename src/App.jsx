95ea9b868d0673a94004bb87e436909c70e3af4e
Vaibhav Kumar Jha <vaibhavkumarjha46@gmail.com>
2024-03-11 18:57:14 +0530

    Reducing State Management & Identifying Unnecessary State

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,29 @@ import GameBoard from "./Components/GameBoard";
 import Log from "./Components/Log";
 import { useState } from "react";
 
+function deriveActivePlayer(gameTurns) {
+  let CurrentPlayer = "X";
+  if (gameTurns.length > 0 && gameTurns[0].player === "X") {
+    CurrentPlayer = "O";
+  }
+  return CurrentPlayer;
+}
+
 function App() {
-  const [activePlayer, setActivePlayer] = useState("X");
+  // const [activePlayer, setActivePlayer] = useState("X");
   const [gameTurns, setGameTurns] = useState([]);
 
+  const activePlayer = deriveActivePlayer(gameTurns);
+
   function handleSelectSquare(rowIndex, colIndex) {
-    setActivePlayer((initialValue) => (initialValue === "X" ? "O" : "X"));
+    // setActivePlayer((initialValue) => (initialValue === "X" ? "O" : "X"));
 
     setGameTurns((prevTurns) => {
-      let CurrentPlayer = "X";
-      if (prevTurns.length > 0 && prevTurns[0].player === "X") {
-        CurrentPlayer = "O";
-      }
+      let CurrentPlayer = deriveActivePlayer(prevTurns);
+      // let CurrentPlayer = "X";
+      // if (prevTurns.length > 0 && prevTurns[0].player === "X") {
+      //   CurrentPlayer = "O";
+      // }
       const updatedTurns = [
         { square: { row: rowIndex, col: colIndex }, player: CurrentPlayer },
         ...prevTurns,
